1aca422fe0f392e0211a9d3e1d4e87b488b2ebf3
bossmaprovjambi-afk <bossmaprovjambi@gmail.com>
2026-06-29 22:57:42 +0700

    Delete sidebar.js

--- sidebar.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector(".toggle-btn");
-const sidebar = document.querySelector(".sidebar");
-
-toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
